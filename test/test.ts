b88263dbf8012a1d66001b0befb2d8093c7bf29b
dddlr <2908767+dddlr@users.noreply.github.com>
2021-08-13 22:59:41 +1200

    Clean up repetitive code in tests

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,8 @@
 import { Match } from '../src/index';
 import { assert } from 'chai';
 
+const GAME_WINNING_POINTS = 4;
+
 describe('Tennis match', () => {
     let match: Match;
 
@@ -178,50 +180,32 @@ describe('Tennis match', () => {
         });
 
         it('should work for a typical match (player 1 wins, 7-5)', () => {
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
+            for (let i = 0; i < 5*GAME_WINNING_POINTS; i++) match.pointWonBy('player 1');
 
             assert.equal(match.score(), '5-0');
 
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
+            for (let i = 0; i < 5*GAME_WINNING_POINTS; i++) match.pointWonBy('player 2');
 
             assert.equal(match.score(), '5-5');
 
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
+            for (let i = 0; i < GAME_WINNING_POINTS; i++) match.pointWonBy('player 1');
             assert.equal(match.score(), '6-5');
 
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
+            for (let i = 0; i < GAME_WINNING_POINTS; i++) match.pointWonBy('player 1');
             assert.equal(match.score(), '7-5, player 1 won match');
         });
 
         it('should work for a tie-break match (6-6)', () => {
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
-
+            for (let i = 0; i < 5*GAME_WINNING_POINTS; i++) match.pointWonBy('player 1');
             assert.equal(match.score(), '5-0');
 
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
-
+            for (let i = 0; i < 5*GAME_WINNING_POINTS; i++) match.pointWonBy('player 2');
             assert.equal(match.score(), '5-5');
 
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 2');
+            for (let i = 0; i < GAME_WINNING_POINTS; i++) match.pointWonBy('player 2');
             assert.equal(match.score(), '5-6');
 
-            for (let i = 0; i < 4; i++) match.pointWonBy('player 1');
+            for (let i = 0; i < GAME_WINNING_POINTS; i++) match.pointWonBy('player 1');
             assert.equal(match.score(), 'tiebreak, 0-0');
 
             // Tie-break begins here
